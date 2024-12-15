9fed2ab85ab4d0ed5ea901387d17093ef044520c
balbes0 <ff347@mail.ru>
2024-12-15 20:46:58 +0300

    ///

--- TestProject3/UnitTest1.cs
+++ TestProject3/UnitTest1.cs
@@ -12,52 +12,48 @@ namespace TestProject3
         }
 
         [TestMethod]
-        public void AreEqualPass()
+        public void CollectionAssert_AreEqual_ShouldPass_WhenCollectionsAreIdentical()
         {
             List<int> testDigits = new List<int> { 1, 2, 3, 4, 5 };
             CollectionAssert.AreEqual(testDigits, digits, "Коллекции не равны");
         }
 
         [TestMethod]
-        public void AreEqualFail()
+        public void CollectionAssert_AreEqual_ShouldFail_WhenCollectionsDiffer()
         {
             List<int> testDigits = new List<int> { 1, 2, 3, 4 };
             CollectionAssert.AreEqual(testDigits, digits, "Коллекции не равны");
         }
 
         [TestMethod]
-        public void AreEquivalentPass()
+        public void CollectionAssert_AreEquivalent_ShouldPass_WhenCollectionsContainSameElements()
         {
             List<int> testDigits = new List<int> { 4, 2, 5, 1, 3 };
-
             CollectionAssert.AreEquivalent(testDigits, digits, "В коллекциях неодинаковые элементы");
         }
 
         [TestMethod]
-        public void AreEquivalentFail()
+        public void CollectionAssert_AreEquivalent_ShouldFail_WhenCollectionsContainDifferentElements()
         {
             List<int> testDigits = new List<int> { 4, 2, 5, 1 };
-
             CollectionAssert.AreEquivalent(testDigits, digits, "В коллекциях неодинаковые элементы");
         }
 
         [TestMethod]
-        public void LastTestPass()
+        public void Assert_AreEqual_ShouldPass_WhenDeltaIsWithinRange()
         {
-            double delta = 0.0001; //разница не более чем ...
-            double result = calculate(77, 142); //два рандомных числа 
-            double trueResult = 54.2254; //округлили до тысячных число 54,22535211267606
-
+            double delta = 0.0001;
+            double result = calculate(77, 142);
+            double trueResult = 54.2254;
             Assert.AreEqual(result, trueResult, delta, "Капутер видимо неправильно посчитал");
         }
 
         [TestMethod]
-        public void LastTestFail()
+        public void Assert_AreEqual_ShouldFail_WhenDeltaExceedsRange()
         {
-            double delta = 0.0001; //разница не более чем ...
-            double result = calculate(77, 142); //два рандомных числа 
-            double trueResult = 54.2255; //округлили неправильно до тысячных число 54,22535211267606
-
+            double delta = 0.0001;
+            double result = calculate(77, 142);
+            double trueResult = 54.2255;
             Assert.AreEqual(result, trueResult, delta, "Капутер видимо неправильно посчитал");
         }
 
